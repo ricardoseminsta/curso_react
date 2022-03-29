e2451bdf1b6a4fb755dbc2a20f8319eb68567e32
RICARDO EMANUEL FERREIRA SILVA <ricardo.emanuel66@gmail.com>
2022-03-29 15:58:03 -0300

    commit  dynamic props

--- 00_first_project/01-react-vite/src/App.tsx
+++ 00_first_project/01-react-vite/src/App.tsx
@@ -1,13 +1,22 @@
 import * as C from './AppStyles'
+import { useState } from 'react';
+
 
 const App = () => {
 
+  const [value, setValue] = useState('#f00');
+
+  const handleClick = () => {
+    setValue('#000')
+  }
 
   return (
     <div>
-      <C.Container>
+      <C.Container bgColor='#CCC'>
         Texto do componente
-        <C.Button>Clique aqui</C.Button>
+        <C.Button bg={value} onClick={handleClick}>Botão Grande</C.Button>
+        <C.Button bg='#0f0' small>Botão Pequeno</C.Button>
+
       </C.Container>
       </div>
   );
--- 00_first_project/01-react-vite/src/AppStyles.ts
+++ 00_first_project/01-react-vite/src/AppStyles.ts
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    background-color: blue;
+type ContainerProps = {
+    bgColor: string;
+}
+export const Container = styled.div<ContainerProps>`
+    background-color: ${props => props.bgColor};
     color: white;
     padding: 20px;
 `;
 
-export const Button = styled.button`
-    font-size: 40px;
+type ButtonProps = {
+    bg: string;
+    small?: boolean;
+}
+export const Button = styled.button<ButtonProps>`
+    font-size: ${props => props.small ? '15px' : '30px'};
+    background-color: ${props => props.bg};
 `;
