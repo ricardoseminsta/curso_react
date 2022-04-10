3076f812a9bd58d1e87df93ad4b309f25e532e8f
Ricardo <ricardo.emanuel66@gmail.com>
2022-04-09 22:24:42 -0300

    commit reducer

--- 00_first_project/01-react-vite/src/App.tsx
+++ 00_first_project/01-react-vite/src/App.tsx
@@ -1,64 +1,45 @@
-import { ChangeEvent, useEffect, useState } from 'react';
-import { Post } from './types/Post'
-import { PostForm } from './components/PostForm';
-import { PostItem } from './components/PostItem';
-import { api } from './api'
+import { useReducer } from "react";
+
+type reducerState = {
+  count: number;
+}
+
+type reducerAction = {
+  type: string;
+
+}
+
+const initialState = { count: 0 };
+const reducer = (state: reducerState, action: reducerAction) => {
+  switch (action.type) {
+    case 'ADD':
+      return {...state, count: state.count + 1};
+    break;
+    case 'DEL':
+      if (state.count > 0) {
+       return {...state, count: state.count - 1};
+    }
+    break
+    case 'RESET':
+      return initialState;
+    break;
+  }
+
+  return state;
+}
 
 const App = () => {
-  const [post, setPost] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadPosts();
-  }, []);
-
-  const loadPosts = async () => {
-    setLoading(true);
-    let json = await api.getAllPosts();
-    setLoading(false);
-    setPost(json);
-
-
-  }
-
-  const handleAddPost = async (title: string, body: string) => {
-    let json = await api.addNewPosts(title, body, 1);
-
-    if (json.id) {
-      alert('Um novo post foi adicionado');
-    } else {
-      alert('Ocorreu algum erro!')
-    }
-  }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="p-5">
-      {loading &&
-        <div>Carregando...</div>
-      }
-
-      <PostForm onAdd={handleAddPost} />
-
-      {!loading && post.length > 0 &&
-        <>
-          Total de Posts: {post.length}
-          <div>
-            {post.map((item, index) => (
-              <PostItem key={index} data={item} />
-            ))}
-          </div>
-        </>
-      }
-
-      {!loading && post.length === 0 &&
-        <div>Tente novamente mais tarde. não há posts para exibir</div>
-      }
-
+      Contagem: {state.count}
+      <hr/>
+      <button className="p-3" onClick={() => dispatch({type: 'ADD'})}>Adicionar</button>
+      <button className="p-3" onClick={() => dispatch({type: 'DEL'})}>Remover</button>
+      <button className="p-3" onClick={() => dispatch({type: 'RESET'})}>Resetar</button>
     </div>
-
   );
 }
 
 export default App;
-
-// https://api.b7web.com.br/cinema/
