4da52dc656b39c8c3bf532d31949aa5728f8c06d
Ricardo <ricardo.emanuel66@gmail.com>
2022-04-18 11:14:06 -0300

    commit redux final styled

--- projetos/redux_exemple/src/App.tsx
+++ projetos/redux_exemple/src/App.tsx
@@ -1,14 +1,41 @@
+import React from 'react';
+import './App.css'
+//import { useSelector } from 'react-redux';
+//import { RootState } from './redux/store'
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from './redux/hooks/useAppSelector'
 
+import { setAge, setName } from './redux/reducers/userReducer'
+import { setThemeStatus, setClass } from './redux/reducers/themeReducer'
 
 function App() {
+  //const user = useSelector((state: RootState) => state.user);
+  const user = useAppSelector(state => state.user);
+  const theme = useAppSelector(state => state.theme);
+  const dispatch = useDispatch();
+
+  const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch( setName(e.target.value) );
+  }
+
+  const handleAgeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch( setAge(parseInt(e.target.value)) );
+  }
+
+  const handleSwitchTehem = () => {   
+    theme.status === 'light' ? dispatch(setThemeStatus('dark')) : dispatch(setThemeStatus('light'));
+    theme.class === 'container' ? dispatch(setClass('container-dark')) : dispatch(setClass('container'));
+  }
+
   return (
-    <div>
-      Meu nome é: ... e tenho ... anos.<br />
-      Tema: ...
+    <div className={theme.class}>
+      Meu nome é: {user.name} e tenho {user.age} anos.<br />
+      Tema: {theme.status}
       <hr />
-      <input type="text" value={'...'} />
+      <input type="text" value={user.name}  onChange={handleNameInput}/>
+      <input type="text" value={user.age}  onChange={handleAgeInput}/>
       <hr />
-      <button>Switch Theme</button>
+      <button onClick={handleSwitchTehem}>Switch Theme</button>
     </div>
   );
 }
