278a626c4c0817bbf841d4f83652a37ca109b9fa
Ricardo <ricardo.emanuel66@gmail.com>
2022-05-30 22:45:38 -0300

    commit sistema login

---
+++ projetos/sistema_login/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Link, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Private } from './pages/Private';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Header do Site</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Página Privada</Link>
+        </nav>
+      </header>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ projetos/sistema_login/src/pages/Private/index.tsx
@@ -0,0 +1,8 @@
+
+export const Private = () => {
+    return (
+        <div>
+            Privada
+        </div>
+    );
+}
